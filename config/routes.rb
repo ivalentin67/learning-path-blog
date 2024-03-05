ddec2f94cda7e39d98fa8f40590e3626241cc559
Iannis VALENTIN <ivalentin@artifrance.fr>
2024-03-05 13:54:19 +0100

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+
+  resources :blog_posts
+
+  # :{nom} = variable
+  # blog_past_path(1) -> "blog_posts/1"
+  # Doit être mis avant sinon new traité comme id
+  #get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  #get "blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  #patch "blog_posts/:id", to: "blog_posts#update"
+  #delete "blog_posts/:id", to: "blog_posts#destroy"
+  #get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  #post "blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  root "blog_posts#index"
+end
